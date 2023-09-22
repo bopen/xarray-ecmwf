725454e4cf1288caf1a9e6f33cd13890810a51f9
Alessandro Amici <a.amici@bopen.eu>
2023-09-22 14:41:38 +0200

    Start the cdsapi chunker

--- xarray_ecmwf/client_cdsapi.py
+++ xarray_ecmwf/client_cdsapi.py
@@ -4,25 +4,42 @@ from typing import Any
 import attrs
 import cdsapi
 
+from . import client_protocol
+
 LOGGER = logging.getLogger(__name__)
 
 SUPPORTED_DATASETS = {"reanalysis-era5-single-levels", "reanalysis-era5-land"}
 
 
-@attrs.define(slots=False)
+@attrs.define
 class CdsapiRequestClient:
     client_kwargs: dict[str, Any] = {"quiet": True, "retry_max": 1}
 
     def submit_and_wait_on_result(self, request: dict[str, Any]) -> Any:
-        request = self.request.copy()
+        request = request.copy()
         dataset = request.pop("dataset")
         if dataset not in SUPPORTED_DATASETS:
-            LOGGER.warning(f"{self.dataset=} not supported")
+            LOGGER.warning(f"{dataset=} not supported")
         client = cdsapi.Client(**self.client_kwargs)
         return client.retrieve(dataset, request | {"format": "grib"})
 
     def get_filename(self, result: Any) -> str:
-        return result.location.split("/")[-1]
+        return result.location.split("/")[-1]  # type: ignore
 
     def download(self, result: Any, target: str | None = None) -> str:
-        return result.download(target)
+        return result.download(target)  # type: ignore
+
+
+@attrs.define(slots=False)
+class CdsapiRequestChunker:
+    request: dict[str, Any]
+    request_chunks: dict[str, Any]
+
+    def get_coords_attrs_and_dtype(
+        self, dataset_cacher=client_protocol.DatasetsCacherProtocol
+    ) -> tuple[dict[str, Any], dict[str, Any], Any]:
+        coords = self.compute_coords()
+        with dataset_cacher.dataset(self.request) as sample_ds:
+            coords["lat"] = ("lat", sample_ds.lat.values, sample_ds.lat.attrs)
+            coords["lon"] = ("lon", sample_ds.lon.values, sample_ds.lon.attrs)
+            return coords, sample_ds.attrs, sample_ds.dtype
