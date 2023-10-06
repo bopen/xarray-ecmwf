bb9c41b3852dc15681d5f989631e865722742362
barghini <a.barghini@bopen.eu>
2023-10-06 10:44:55 +0200

    fix in test request for openadata oper

--- tests/integration_test_40_ecmwf_opendata_oper.py
+++ tests/integration_test_40_ecmwf_opendata_oper.py
@@ -8,12 +8,11 @@ LOGGER = logging.getLogger(__name__)
 
 REQUEST = {
     "source": "ecmwf",
-    "type": "pf",
-    "param": ["msl"],
+    "type": "fc",
+    "param": ["2t"],
     "date": -1,
     "step": ["12", "24", "48"],
     "time": ["0", "12"],
-    "number": [1, 2],
 }
 
 
@@ -21,7 +20,7 @@ def test_open_dataset() -> None:
     res = xr.open_dataset(REQUEST, engine="ecmwf", client="ecmwf-opendata")  # type: ignore
 
     assert isinstance(res, xr.Dataset)
-    assert set(res.dims) == {"time", "step", "latitude", "longitude", "number"}
+    assert set(res.dims) == {"time", "step", "latitude", "longitude"}
     assert res.time.size == 2
     assert res.step.size == 3
 
@@ -33,10 +32,10 @@ def test_compare_chunked_no_chunked_values() -> None:
         REQUEST,  # type: ignore
         engine="ecmwf",
         client="ecmwf-opendata",
-        request_chunks={"step": 1, "number": 1},
+        request_chunks={"step": 1},
         chunks={},
     )
-    res1 = ds1.data_vars["msl"].load()
+    res1 = ds1.data_vars["2t"].load()
 
     ds2 = xr.open_dataset(
         REQUEST,  # type: ignore
@@ -44,9 +43,9 @@ def test_compare_chunked_no_chunked_values() -> None:
         client="ecmwf-opendata",
         chunks={},
     )
-    res2 = ds2.data_vars["msl"].load()
+    res2 = ds2.data_vars["2t"].load()
 
-    assert (res1 - res2).shape == (2, 3, 2, 451, 900)
+    assert (res1 - res2).shape == (2, 3, 451, 900)
     assert (res1 == res2).all()
 
 
@@ -56,15 +55,15 @@ def test_cds_era5_single_time() -> None:
         engine="ecmwf",
         client="ecmwf-opendata",
         chunks={},
-        request_chunks={"step": 1, "number": 1},
+        request_chunks={"step": 1},
     )
-    da = ds.data_vars["msl"]
+    da = ds.data_vars["2t"]
 
     time = datetime.date.today() - datetime.timedelta(days=1)
     res = da.sel(time=f"{time}T00:00").compute()
 
     assert isinstance(res, xr.DataArray)
-    assert set(res.dims) == {"step", "latitude", "longitude", "number"}
+    assert set(res.dims) == {"step", "latitude", "longitude"}
 
     res = da.sel(time=f"{time}T00:00").mean().compute()
 
@@ -77,9 +76,9 @@ def test_cds_era5_small_slice_time() -> None:
         engine="ecmwf",
         client="ecmwf-opendata",
         chunks={},
-        request_chunks={"step": 1, "number": 1},
+        request_chunks={"step": 1},
     )
-    da = ds.data_vars["msl"]
+    da = ds.data_vars["2t"]
 
     time = datetime.date.today() - datetime.timedelta(days=1)
     res = da.sel(time=f"{time}").compute()
@@ -99,15 +98,15 @@ def test_cds_era5_small_step() -> None:
         engine="ecmwf",
         client="ecmwf-opendata",
         chunks={},
-        request_chunks={"step": 1, "number": 1},
+        request_chunks={"step": 1},
     )
-    da = ds.data_vars["msl"]
+    da = ds.data_vars["2t"]
 
     datetime.date.today() - datetime.timedelta(days=1)
     res = da.sel(step=datetime.timedelta(hours=12)).compute()
 
     assert isinstance(res, xr.DataArray)
-    assert set(res.dims) == {"time", "latitude", "longitude", "number"}
+    assert set(res.dims) == {"time", "latitude", "longitude"}
 
     res = da.sel(step=datetime.timedelta(hours=12)).mean().compute()
 
@@ -120,9 +119,9 @@ def test_cds_era5_small_slice_step() -> None:
         engine="ecmwf",
         client="ecmwf-opendata",
         chunks={},
-        request_chunks={"step": 1, "number": 1},
+        request_chunks={"step": 1},
     )
-    da = ds.data_vars["msl"]
+    da = ds.data_vars["2t"]
 
     datetime.date.today() - datetime.timedelta(days=1)
 
@@ -131,7 +130,7 @@ def test_cds_era5_small_slice_step() -> None:
     res = da.sel(step=step_slice).compute()
 
     assert isinstance(res, xr.DataArray)
-    assert set(res.dims) == {"time", "step", "latitude", "longitude", "number"}
+    assert set(res.dims) == {"time", "step", "latitude", "longitude"}
     assert res.step.size == 2
 
 
@@ -141,10 +140,10 @@ def test_compare_chunked_no_chunked() -> None:
         REQUEST,  # type: ignore
         engine="ecmwf",
         client="ecmwf-opendata",
-        request_chunks={"step": 1, "number": 1},
+        request_chunks={"step": 1},
         chunks={},
     )
-    res1 = ds1.data_vars["msl"].load()
+    res1 = ds1.data_vars["2t"].load()
 
     ds2 = xr.open_dataset(
         REQUEST,  # type: ignore
@@ -152,6 +151,6 @@ def test_compare_chunked_no_chunked() -> None:
         client="ecmwf-opendata",
         chunks={},
     )
-    res2 = ds2.data_vars["msl"].load()
+    res2 = ds2.data_vars["2t"].load()
 
     assert res1.equals(res2)
