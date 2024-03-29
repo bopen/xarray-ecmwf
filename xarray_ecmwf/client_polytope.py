f2d90a447b36cd90e97355967945d8cd20aaf782
Alessandro Amici <a.amici@bopen.eu>
2024-03-29 23:17:49 +0100

    Add locking to prevent the dreaded concurrency bug and a download optimization

--- xarray_ecmwf/client_polytope.py
+++ xarray_ecmwf/client_polytope.py
@@ -5,6 +5,7 @@ from typing import Any
 
 import attrs
 import polytope.api
+import xarray as xr
 
 LOGGER = logging.getLogger(__name__)
 
@@ -14,21 +15,27 @@ CLIENT_KWARGS_DEFAULTS = {"quiet": True, "verbose": False}
 @attrs.define
 class PolytopeRequestClient:
     client_kwargs: dict[str, Any] = {}
+    retrieve_lock = xr.backends.locks.get_write_lock("polytope-retrieve")  # type: ignore
+    download_lock = xr.backends.locks.get_write_lock("polytope-downlaod")  # type: ignore
 
     def submit_and_wait_on_result(self, request: dict[str, Any]) -> Any:
         path = hashlib.md5(str(request).encode("utf-8")).hexdigest() + ".grib"
         client = polytope.api.Client(**CLIENT_KWARGS_DEFAULTS | self.client_kwargs)
-        res = client.retrieve("destination-earth", request, path, asynchronous=True)[0]
-        # the following doesn't downloads, it just waits until the result is ready
-        res.download(pointer=True)
-        return res
+        # polytope-server appears not to support concurrent resolution=high requests
+        with self.retrieve_lock:
+            res = client.retrieve("destination-earth", request, path, asynchronous=True)
+            # the following doesn't downloads, it just waits until the result is ready
+            res[0].download(pointer=True)
+        return res[0]
 
     def get_filename(self, result: Any) -> str:
         return result.output_file  # type: ignore
 
     def download(self, result: Any, target: str | None = None) -> str:
         assert target is not None
-        result.download(output_file=target)
+        # this is an optimization of network bandwidth
+        with self.download_lock:
+            result.download(output_file=target)
         if os.stat(target).st_size == 0:
             request = result.describe()["user_request"]
             raise TypeError(f"polytope returned an empty file: {target} for {request}")
